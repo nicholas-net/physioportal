5336a51764229ff0f01c31bd5f5fb92f2cf5d3aa
nicholas-net <cloudvibeclothing@gmail.com>
2025-10-06 12:18:45 -0400

    refactor: return html template for login page request

--- portal/views.py
+++ portal/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 def login_view(request):
     """
-    Handles requests for the home page resource.
+    Users are routed to the log-in page for authentication
 
     Args:
         request: HttpRequest object
 
     Returns:
-        HttResponse object for the portal homepage
+        HttResponse object for the portal log-in page
     """
-    return HttpResponse("login page/homepage")
+
+    template = loader.get_template("login.html")
+    return HttpResponse(template.render())
