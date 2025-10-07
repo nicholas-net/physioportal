73c50f5fb6e460832151e21190687b7fa0da52f4
nicholas-net <cloudvibeclothing@gmail.com>
2025-10-06 21:38:04 -0400

    feat: add patient ORM model and initial migration

--- portal/models.py
+++ portal/models.py
@@ -1,3 +1,21 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+# Patient inherits the functionalities to interact with the database
+class Patient(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    date_of_birth = models.DateField()
+    medical_history = models.CharField(max_length=255)
+
+    # Establish one to one relationship with user model
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+
+
+class Exercises(models.Model):
+    exercise_name = models.CharField(max_length=255)
+
+
+
+
