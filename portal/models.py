9f9eb77a6c92533eb9b5b1877b5e9bf5acb95ecd
nicholas-net <cloudvibeclothing@gmail.com>
2025-10-07 18:34:28 -0400

    feat : link patient and exercise models with program to establish a many-to-many relationship

--- portal/models.py
+++ portal/models.py
@@ -1,5 +1,11 @@
 from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models import CASCADE
+
+
+class Exercise(models.Model):
+    name = models.CharField(max_length=255)
 
 
 # Patient inherits the functionalities to interact with the database
@@ -9,13 +15,25 @@ class Patient(models.Model):
     date_of_birth = models.DateField()
     medical_history = models.CharField(max_length=255)
 
-    # Establish one to one relationship with user model
+    # Establish one-to-one relationship with user model
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    # Program hasn't been defined yet so program is referenced lazily
+    programs = models.ManyToManyField(Exercise, through="Program")
 
+# negative set and rep counts doesn't make any sense
+def validate_min(value):
+    if value < 0:
+        raise ValidationError(f"{value} is not valid")
 
-class Exercises(models.Model):
-    exercise_name = models.CharField(max_length=255)
-
-
+class Program(models.Model):
+    """
+    Deleting a patient or exercise will result in related records
+    linked to that model being deleted
+    """
+    patient = models.ForeignKey(Patient, on_delete=CASCADE)
+    exercise = models.ForeignKey(Exercise, on_delete=CASCADE)
+    sets = models.IntegerField(validators=[validate_min])
+    reps = models.IntegerField(validators=[validate_min])
+    position = models.CharField(max_length=255)
 
 
